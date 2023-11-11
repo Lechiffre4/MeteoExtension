a3dceef44dfd1e8ba396da57441642ef7bb6228f
lechiffre4 <toxcoa@gmail.com>
2023-11-11 02:27:34 -0500

    Display current data for montréal

--- src/popup.jsx
+++ src/popup.jsx
@@ -1,30 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { render } from "react-dom";
-import { useState, useEffect } from "react";
 
 function Popup() {
-	const [Temperature, setTemperature] = useState(null);
+  const [currentData, setCurrentData] = useState(null);
 
+  useEffect(() => {
+    fetch(
+      "https://bewildered-pleat-foal.cyclic.app/api/currentweather/qc/147",
+      {
+        method: "GET",
+      }
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setCurrentData(data);
+        console.log(data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
 
-	useEffect(() => {
-		console.log("test");
-		fetch("https://bewildered-pleat-foal.cyclic.app/weather/147", {
-			method: "GET",
-		})
-			.then((response) => response.json())
-			.then((data) => {
-				setTemperature(JSON.stringify(data));
-				console.log(data);
-			})
-			.catch((error) => console.log(error));
-	}, []);
-
-	return (
-		<div>
-			<h2>Data:</h2>
-			<p>{Temperature}</p>
-		</div>
-	);
+  return (
+    <div>
+      <h2>Data:</h2>
+      {currentData && (
+        <div>
+          <p>City: {currentData.City}</p>
+          <p>Temperature (Metric): {currentData.TempMetric}</p>
+          <p>Temperature (Imperial): {currentData.TempImperial}</p>
+          <p>Humidity: {currentData.humidity}</p>
+          <img src={currentData.iconURL} alt="Weather Icon" />
+          <p>Timestamp: {currentData.timeStamp}</p>
+          <p>Wind Speed (Metric): {currentData.WindSpeedMetric}</p>
+          <p>Wind Speed (Imperial): {currentData.WindSpeedImperial}</p>
+          <p>Wind Direction: {currentData.WindDirection}</p>
+        </div>
+      )}
+    </div>
+  );
 }
 
 render(<Popup />, document.getElementById("react-target"));
+
+
+
+
+
+
